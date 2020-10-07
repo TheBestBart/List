6fb702a9cef985ab42c45ee1d82487372f189f29
Bartek <bart.lebek@o2.pl>
2020-10-08 00:20:30 +0200

    Title Component created

---
+++ list/src/components/ui/Titile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title: React.SFC = () => {
+    return ( 
+        <div className='title'>
+            Kurs Walut
+        </div>
+    );
+}
+ 
+export default Title;
